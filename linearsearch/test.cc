07e52b72b3ad510505755fc75602644efd05ba4e
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2015-10-03 23:57:14 -0500

    c++ done

---
+++ linearsearch/test.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  int x = 5;
+  int z [7] = {1,2,3,4,5,6,7};
+  int i;
+
+  for (i=0; i<x; i++) {
+    if (x == z[i]) {
+      std::cout << i;
+      std::cout << std::endl;
+    }
+  }
+}
